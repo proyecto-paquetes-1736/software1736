5ab341583e3d71c550317ea282a35f500a8c6bce
rvanegas2022 <104838269+rvanegas2022@users.noreply.github.com>
2022-09-22 21:33:30 -0500

    estructura del proyecto
    primer commit con la estructura del proyecto

---
+++ proyecto1736/src/main/java/com/example/proyecto1736/Proyecto1736Application.java
@@ -0,0 +1,13 @@
+package com.example.proyecto1736;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1736Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1736Application.class, args);
+	}
+
+}
---
+++ proyecto1736/src/test/java/com/example/proyecto1736/Proyecto1736ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto1736;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1736ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
